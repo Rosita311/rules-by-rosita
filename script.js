5cf5d065a2ff5d459fc042de18cb53c3a2470f75
Rosita <rosita-rampertaap@kpnmail.nl>
2025-03-15 20:50:33 +0100

    Add darkmode, not finished yet

--- script.js
+++ script.js
@@ -38,3 +38,31 @@ navLinks.forEach(link => {
 });
 
 updateNavbar(media);
+
+// Darkmode
+let darkmode = localStorage.getItem('darkmode');
+const themeSwitch = document.getElementById('theme-switch');
+
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkmode', 'active');
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkmode', null);
+}
+
+if(darkmode === 'active') {
+    enableDarkmode();
+} else {
+    disableDarkmode();
+}
+
+themeSwitch.addEventListener('click', () => {
+    if (darkmode !== 'active') {
+        enableDarkmode();
+    } else {
+        disableDarkmode();
+    }
+});
