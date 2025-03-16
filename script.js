e61bc3f45c3faa565d64e0102e356bd1a90b8e8f
Rosita <rosita-rampertaap@kpnmail.nl>
2025-03-16 15:39:08 +0100

    add workig darkmode buttons

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Menu
 const openButton = document.getElementById('open-sidebar-button');
 const navbar = document.getElementById("navbar");
 
@@ -40,17 +41,26 @@ navLinks.forEach(link => {
 updateNavbar(media);
 
 // Darkmode
+
 let darkmode = localStorage.getItem('darkmode');
-const themeSwitch = document.getElementById('theme-switch');
+const themeSwitchButtons = document.querySelectorAll('.theme-switch');
 
 const enableDarkmode = () => {
+    console.log('Darkmode is ingeschakeld');
     document.body.classList.add('darkmode');
     localStorage.setItem('darkmode', 'active');
+    themeSwitchButtons.forEach(button => {
+        button.setAttribute('aria-label', 'Schakel over naar licht thema');
+    });
 }
 
 const disableDarkmode = () => {
+    console.log('Darkmode is uitgeschakeld');
     document.body.classList.remove('darkmode');
-    localStorage.setItem('darkmode', null);
+    localStorage.setItem('darkmode', 'inactive');
+    themeSwitchButtons.forEach(button => {
+        button.setAttribute('aria-label', 'Schakel over naar donker thema');
+    });
 }
 
 if(darkmode === 'active') {
@@ -59,11 +69,14 @@ if(darkmode === 'active') {
     disableDarkmode();
 }
 
-themeSwitch.addEventListener('click', () => {
-    if (darkmode !== 'active') {
-        enableDarkmode();
-    } else {
-        disableDarkmode();
-    }
+themeSwitchButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        console.log("Darkmode knop is ingedrukt!");
+        darkmode = localStorage.getItem('darkmode');
+        if (darkmode !== 'active') {
+            enableDarkmode();
+        } else {
+            disableDarkmode();
+        }
+    });
 });
-// https://www.youtube.com/watch?v=_gKEUYarehE
