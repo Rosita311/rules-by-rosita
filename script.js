657ea135d00e943fde5b81b56a2921b110b5ca5b
Rosita <rosita-rampertaap@kpnmail.nl>
2025-03-18 21:41:06 +0100

    Finish header and darkmode

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ updateNavbar(media);
 // Darkmode
 
 document.addEventListener('DOMContentLoaded', () => {
-    const themeSwitchButtons = document.querySelectorAll('.theme-switch');
+    const themeSwitch = document.getElementById('theme-switch');
     let darkmode = localStorage.getItem('darkmode');
     const root = document.documentElement; 
 
@@ -51,21 +51,17 @@ document.addEventListener('DOMContentLoaded', () => {
         root.classList.add('darkmode');
         root.style.colorScheme = 'dark';
         localStorage.setItem('darkmode', 'active');
-        themeSwitchButtons.forEach(button => {
-            button.setAttribute('aria-label', 'Schakel over naar licht thema');
-            button.setAttribute('aria-pressed', 'true');
-        });
-    }
+        themeSwitch.setAttribute('aria-label', 'Schakel over naar licht thema');
+        themeSwitch.setAttribute('aria-pressed', 'true');
+        };
 
     const disableDarkmode = () => {
         root.classList.remove('darkmode');
         root.style.colorScheme = 'light';
         localStorage.setItem('darkmode', 'inactive' );
-        themeSwitchButtons.forEach(button => {
-            button.setAttribute('aria-label', 'Schakel over naar donker thema');
-            button.setAttribute('aria-pressed', 'false');
-        });
-    }
+        themeSwitch.setAttribute('aria-label', 'Schakel over naar donker thema');
+        themeSwitch.setAttribute('aria-pressed', 'false');
+        };
 
     if(darkmode === 'active') {
         enableDarkmode();
@@ -87,15 +83,13 @@ if(!darkmode) {
     }
 }
 
-    themeSwitchButtons.forEach(button => {
-        button.addEventListener('click', () => {
+ themeSwitch.addEventListener('click', () => {
             darkmode = localStorage.getItem('darkmode');
             if (darkmode !== 'active') {
                 enableDarkmode();
             } else {
                 disableDarkmode();
             }
-        });
-    })
-
-});
+        })
+    }
+);
