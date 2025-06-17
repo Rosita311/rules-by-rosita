470767fd43e8b78a83dc4c5e956f1db12ee00935
Rosita <rosita-rampertaap@kpnmail.nl>
2025-06-17 13:31:36 +0200

    Add comment section with conditional display in single post template

--- single.php
+++ single.php
@@ -207,6 +207,12 @@ get_template_part('template-parts/accessibility-panel'); ?>
             </div>
             <div class="post-comments">
               <h2>Reacties</h2>
+              <?php
+              if (comments_open() || get_comments_number()) {
+                comments_template();
+              } else {
+                echo '<p>Reacties zijn gesloten voor dit bericht.</p>';
+              } ?>
               <div class="comment-form">
                 <form
                   action="post-comment.php"
