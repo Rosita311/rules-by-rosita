4781808ee261e61087f3829aecc2af7bf9ac8f79
Rosita <rosita-rampertaap@kpnmail.nl>
2025-06-21 16:48:40 +0200

    Refactor sidebar implementation: move sidebar code to separate file and update single post layout

--- single.php
+++ single.php
@@ -198,13 +198,7 @@ get_template_part('template-parts/accessibility-panel'); ?>
         </div>
 
       </article>
-      <?php if (is_active_sidebar('main-sidebar')) : ?>
-        <aside class="sidebar">
-          <div class="sidebar-inner">
-            <?php dynamic_sidebar('main-sidebar'); ?>
-          </div>
-        </aside>
-      <?php endif; ?>
+     <?php get_sidebar('single'); ?>
     </section>
     <section class="blogpost-section">
       <h2>Gerelateerde blogposts</h2>
