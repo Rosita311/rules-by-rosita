5856721b7c525b8210b5e604c6e0f3b53b1e6709
Rosita <rosita-rampertaap@kpnmail.nl>
2025-07-03 13:57:32 +0200

    Enhance submenu accessibility: add padding to focus states and improve submenu toggle button labeling

--- inc/class-rules-walker-nav-menu.php
+++ inc/class-rules-walker-nav-menu.php
@@ -1,9 +1,11 @@
 <?php
 
-class Rules_Walker_Nav_Menu extends Walker_Nav_Menu {
+class Rules_Walker_Nav_Menu extends Walker_Nav_Menu
+{
   private $current_item;
 
-  function start_el(&$output, $item, $depth = 0, $args = [], $id = 0) {
+  function start_el(&$output, $item, $depth = 0, $args = [], $id = 0)
+  {
     $classes = empty($item->classes) ? [] : (array) $item->classes;
     $has_children = in_array('menu-item-has-children', $classes);
     $is_current = in_array('current-menu-item', $classes);
@@ -20,9 +22,9 @@ class Rules_Walker_Nav_Menu extends Walker_Nav_Menu {
       $output .= '<button class="submenu-toggle"';
       $output .= ' aria-haspopup="true"';
       $output .= ' aria-expanded="false"';
-      $output .= ' aria-controls="' . esc_attr($submenu_id) . '"';
-      $output .= ' aria-label="Submenu openen">';
+      $output .= ' aria-controls="' . esc_attr($submenu_id) . '">';
       $output .= esc_html($item->title);
+      $output .= '<span class="sr-only"> submenu openen</span>';
       $output .= '<span class="chevron" aria-hidden="true">';
       $output .= '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-chevron-down"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 9l6 6l6 -6" /></svg>';
       $output .= '</span>';
@@ -37,16 +39,19 @@ class Rules_Walker_Nav_Menu extends Walker_Nav_Menu {
     }
   }
 
-  function end_el(&$output, $item, $depth = 0, $args = []) {
+  function end_el(&$output, $item, $depth = 0, $args = [])
+  {
     $output .= '</li>';
   }
 
-  function start_lvl(&$output, $depth = 0, $args = [], $id = 0) {
+  function start_lvl(&$output, $depth = 0, $args = [], $id = 0)
+  {
     $submenu_id = 'submenu-' . $this->current_item->ID;
     $output .= '<ul class="submenu" id="' . esc_attr($submenu_id) . '" aria-hidden="true" tabindex="-1">';
   }
 
-  function end_lvl(&$output, $depth = 0, $args = []) {
+  function end_lvl(&$output, $depth = 0, $args = [])
+  {
     $output .= '</ul>';
   }
 }
