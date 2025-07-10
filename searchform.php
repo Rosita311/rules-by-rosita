a1e20405447848092b2d0b24d6c819c6b80dec59
Rosita <rosita-rampertaap@kpnmail.nl>
2025-07-10 20:31:32 +0200

    try ariadescribedby

--- searchform.php
+++ searchform.php
@@ -1,6 +1,9 @@
  <form role="search" method="get" action="<?php echo esc_url(home_url('/')); ?>" class="site-form">
    <div class="form-group">
      <label id="search-label" for="search-input" class="search-label">Zoeken</label>
+     <!-- <p id="search-instructions" class="sr-only">
+       Begin met typen en druk op Enter om te zoeken.
+     </p> -->
      <div class="form-group-row">
        <input
          id="search-input"
@@ -8,7 +11,8 @@
          type="search"
          name="s"
          placeholder="Typ wat je wilt vinden."
-         required />
+         required 
+         />
        <button type="submit" class="btn btn-secondary">Zoeken</button>
      </div>
    </div>
