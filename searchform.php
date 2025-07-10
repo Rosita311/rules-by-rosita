22be67f7008f9a817710702cbca19725a2d2bd1f
Rosita <rosita-rampertaap@kpnmail.nl>
2025-07-10 20:05:39 +0200

    add placeholder styles

--- searchform.php
+++ searchform.php
@@ -1,20 +1,20 @@
  <form role="search" method="get" action="<?php echo esc_url(home_url('/')); ?>" class="site-form">
-    <div class="form-group">
-      <p id="search-instructions" class="sr-only">
-  Typ een zoekterm en druk op Enter om te zoeken.
-</p>
-      <label id="search-label" for="search-input" class="search-label">Zoeken</label>
-      <div class="form-group-row">
-        <input
-          id="search-input"
-          class="search-input"
-          type="search"
-          name="s"
-          placeholder="Waar ben je naar op zoek?"
-          required
-           aria-describedby="search-instructions"/>
+   <div class="form-group">
+     <p id="search-instructions" class="sr-only">
+       Begin met typen en druk op Enter om te zoeken.
+     </p>
+     <label id="search-label" for="search-input" class="search-label">Zoeken</label>
+     <div class="form-group-row">
+       <input
+         id="search-input"
+         class="search-input"
+         type="search"
+         name="s"
+         placeholder="Typ wat je wilt vinden."
+         required
+         aria-describedby="search-instructions" />
 
-        <button type="submit" class="btn btn-secondary">Zoeken</button>
-      </div>
-    </div>
-  </form>
+       <button type="submit" class="btn btn-secondary">Zoeken</button>
+     </div>
+   </div>
+ </form>
