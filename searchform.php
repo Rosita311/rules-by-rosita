3f823f985793bcac86bc6e90144401331b562632
Rosita <rosita-rampertaap@kpnmail.nl>
2025-07-10 17:02:15 +0200

    add search instructions

--- searchform.php
+++ searchform.php
@@ -1,5 +1,8 @@
  <form role="search" method="get" action="<?php echo esc_url(home_url('/')); ?>" class="site-form">
     <div class="form-group">
+      <p id="search-instructions" class="sr-only">
+  Typ een zoekterm en druk op Enter om te zoeken.
+</p>
       <label id="search-label" for="search-input" class="search-label">Zoeken</label>
       <div class="form-group-row">
         <input
@@ -9,7 +12,7 @@
           name="s"
           placeholder="Waar ben je naar op zoek?"
           required
-          aria-describedby="search-instructions" />
+           aria-describedby="search-instructions"/>
 
         <button type="submit" class="btn btn-secondary">Zoeken</button>
       </div>
