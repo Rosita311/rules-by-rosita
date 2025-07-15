baf9096f4ca1752deb303cf62a3de5d73d87d50e
Rosita <rosita-rampertaap@kpnmail.nl>
2025-07-15 17:45:36 +0200

    wrap search form in <search> tag for improved structure

--- searchform.php
+++ searchform.php
@@ -1,19 +1,16 @@
- <form role="search" method="get" action="<?php echo esc_url(home_url('/')); ?>" class="site-form">
-   <div class="form-group">
-     <label id="search-label" for="search-input" class="search-label">Zoeken</label>
-     <!-- <p id="search-instructions" class="sr-only">
-       Begin met typen en druk op Enter om te zoeken.
-     </p> -->
-     <div class="form-group-row">
-       <input
-         id="search-input"
-         class="search-input"
-         type="search"
-         name="s"
-         placeholder="Typ wat je wilt vinden."
-         required 
-         />
-       <button type="submit" class="btn btn-secondary">Zoeken</button>
+ <search>
+   <form role="search" method="get" action="<?php echo esc_url(home_url('/')); ?>" class="site-form">
+     <div class="form-group">
+       <label id="search-label" for="search-input" class="search-label">Zoeken</label>
+       <div class="form-group-row">
+         <input
+           id="search-input"
+           class="search-input"
+           type="search"
+           name="s"
+           placeholder="Typ wat je wilt vinden." />
+         <button type="submit" class="btn btn-secondary">Zoeken</button>
+       </div>
      </div>
-   </div>
- </form>
+   </form>
+ </search>
