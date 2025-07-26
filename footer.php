0ba70bb243741002e2ac954804880905fb96bba6
Rosita <rosita-rampertaap@kpnmail.nl>
2025-07-26 18:13:36 +0200

    Remove hardcoded items from footer

--- footer.php
+++ footer.php
@@ -14,38 +14,37 @@
         </nav>
       </div>
       <div class="footer-widgets-container">
-          <?php if (is_active_sidebar('footer-widgets')) : ?>
-            <div class="footer-widgets">
-              <?php dynamic_sidebar('footer-widgets'); ?>
-            </div>
-          <?php endif; ?>
+        <?php if (is_active_sidebar('footer-widgets')) : ?>
+          <div class="footer-widgets">
+            <?php dynamic_sidebar('footer-widgets'); ?>
+          </div>
+        <?php endif; ?>
       </div>
     </div>
-  </div>
-  <div class="footer-info">
-    <div
-      class="footer-plumbob"
-      tabindex="0"
-      role="button"
-      aria-describedby="plumbob-desc">
-      <svg
-        viewBox="0 0 100 160"
-        xmlns="http://www.w3.org/2000/svg"
-        aria-hidden="true">
-        <polygon
-          points="50,0 90,80 50,160 10,80"
-          class="plumbob-shape" />
-      </svg>
-      <span
-        id="plumbob-desc"
-        class="sr-only">Sims plumbob animatie zegt: Sul sul!</span>
-      <span
-        class="plumbob-easteregg"
-        aria-hidden="true"
-        hidden>Sul sul!</span>
+    <div class="footer-info">
+      <div
+        class="footer-plumbob"
+        tabindex="0"
+        role="button"
+        aria-describedby="plumbob-desc">
+        <svg
+          viewBox="0 0 100 160"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true">
+          <polygon
+            points="50,0 90,80 50,160 10,80"
+            class="plumbob-shape" />
+        </svg>
+        <span
+          id="plumbob-desc"
+          class="sr-only">Sims plumbob animatie zegt: Sul sul!</span>
+        <span
+          class="plumbob-easteregg"
+          aria-hidden="true"
+          hidden>Sul sul!</span>
+      </div>
+      <p>&copy; <?php echo date('Y'); ?> Rules by Rosita. All rights reserved.</p>
     </div>
-    <p>&copy; <?php echo date('Y'); ?> Rules by Rosita. All rights reserved.</p>
-  </div>
   </div>
 </footer>
 </body>
