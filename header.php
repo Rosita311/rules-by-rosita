cca8e9b2583d7f9b327247bef1166be0fe0f8ca3
Rosita <rosita-rampertaap@kpnmail.nl>
2026-01-02 14:02:41 +0100

    Enhance menu fallback by adding back-to-top button and refining menu close button class

--- header.php
+++ header.php
@@ -67,7 +67,7 @@
         </a>
         <a
           href="<?php echo esc_url(remove_query_arg('menu')); ?>"
-          class=" no-js menu-close btn-icon-small"
+          class=" no-js btn-icon-small"
           id="menu-close-toggle-no-js"
           aria-label="Sluit menu">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-x">
