70f88bebae4337f7cdf49c4d9ce716e25e14e733
Rosita <64226867+Rosita311@users.noreply.github.com>
2026-06-02 21:50:24 +0200

    feat: update editor styles to include custom font styles in the editor

--- functions.php
+++ functions.php
@@ -17,6 +17,7 @@ function rules_by_rosita_setup() {
     add_theme_support( 'title-tag' );
     add_theme_support( 'post-thumbnails' );
     add_theme_support( 'editor-styles' );
+    add_editor_style( RULES_BY_ROSITA_FONTS_URL );
     add_editor_style( 'editor-style.css' );
     add_theme_support( 'automatic-feed-links' );
     add_theme_support( 'wp-block-styles' );
