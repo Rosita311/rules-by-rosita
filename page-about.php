a081df9c015d1257d41332f2286a4422b2535dec
Rosita <64226867+Rosita311@users.noreply.github.com>
2026-06-02 21:24:57 +0200

    feat: create About page template with accessibility panel and content sections

---
+++ page-about.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Template Name: Over mij
+ */
+get_header();
+get_template_part('template-parts/accessibility-panel'); ?>
+<main id="main-content">
+  <?php get_template_part('template-parts/page-hero'); ?>
+  <div class="container-main">
+
+    <?php get_template_part('template-parts/introduction'); ?>
+
+    <section class="entry-content">
+      <?php the_content(); ?>
+    </section>
+
+    <?php get_template_part('template-parts/back-to-top'); ?>
+  </div>
+</main>
+<?php get_footer(); ?>
